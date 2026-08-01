ce2d08eda068d1da5110fb18ed7820604d27d351
hugoattal <4563971+hugoattal@users.noreply.github.com>
2026-08-01 22:42:11 +0200

    Add icon and fx

--- src/components/navigation.ts
+++ src/components/navigation.ts
@@ -2,23 +2,13 @@ import type { ArrayOrNested, NavigationMenuItem } from "@nuxt/ui";
 
 export const navigationItems: ArrayOrNested<NavigationMenuItem> = [
     {
-        icon: "lucide:calendars",
-        label: "Chronologie",
-        to: "/timeline"
+        icon: "lucide:chess-king",
+        label: "Landing",
+        to: "/"
     },
     {
-        icon: "lucide:book",
-        label: "Contribuer",
-        to: "/contribute"
-    },
-    {
-        icon: "lucide:info",
-        label: "À propos",
-        to: "/about"
-    },
-    {
-        icon: "lucide:pencil-ruler",
-        label: "Éditeur",
-        to: "/editor"
+        icon: "bx:bxs-chess",
+        label: "Board",
+        to: "/board"
     }
 ];
---
+++ src/pages/board/lib/pieces.ts
@@ -0,0 +1,10 @@
+import { BISHOP, KING, KNIGHT, PAWN, QUEEN, ROOK } from "chess.js";
+
+export const pieces = {
+    [BISHOP]: "lucide:chess-bishop",
+    [KING]: "lucide:chess-king",
+    [KNIGHT]: "lucide:chess-knight",
+    [PAWN]: "lucide:chess-pawn",
+    [QUEEN]: "lucide:chess-queen",
+    [ROOK]: "lucide:chess-rook"
+};
